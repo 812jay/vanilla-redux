d1e3a4f6c9b0fef5489ea99b4f85c00fbe12dcde
812jay <yongje812@gmail.com>
2021-01-28 14:03:02 +0900

    #3.2 mapStateToProps

--- src/store.js
+++ src/store.js
@@ -20,9 +20,9 @@ export const deleteToDo = (id) => {
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD:
-      return [{ text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE:
-      return state.filter(toDo !== action.id);
+      return state.filter((toDo) => toDo !== action.id);
     default:
       return state;
   }
